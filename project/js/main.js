8084635a0db884b6477dc93302cf4dec0fb952f5
Justine Dezember <justinedezember@wlan-172-20-62-129.csuohio.edu>
2019-12-03 16:59:36 -0500

    weather app

--- project/js/main.js
+++ project/js/main.js
@@ -1,76 +1,23 @@
-import fetchJsonp from 'fetch-jsonp';
-import { isValidZip, showAlert } from './validate.js';
+function getWeather(searchQuery) {
+  var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=imperial&APPID="+apiKey;
 
-const petForm = document.querySelector('#pet-form')
+  $(".city").text("");
+  $(".temp").text("");
+  $(".pressure").text("");
+    $(".wind").text("");
 
-petForm.addEventListener('submit', fetchAnimals)
-
-// Fetch Animals From API
-function fetchAnimals(e) {
-  e.preventDefault()
-
-  // Get User Input
-  const animal = document.querySelector('#animal').value
-  const zip = document.querySelector('#zip').value
-
-  // Validate Zip
-  if (!isValidZip(zip)) {
-    showAlert('Please Enter A Valid Zipcode', 'danger')
-    return
-  }
-
-  // Fetch Pets
-  fetchJsonp(
-    `https://api.petfinder.com/pet.find?format=json&key=SH4WG8mWg11bZgS06axwXRKL93Vx4Vl09ZujESuudb95kdbvtQ&animal=${animal}&location=${zip}&callback=callback`,
-    {
-      jsonpCallbackFunction: 'callback',
-    }
-  )
-    .then((res) => res.json())
-    .then((data) => showAnimals(data.petfinder.pets.pet))
-    .catch((err) => console.log(err))
+  $.ajax(url,{success: function(data){
+    $(".city").text("City: "+data.name);
+    $(".temp").text("Temperature: "+data.main.temp+" *F");
+    $(".pressure").text("Pressure: "+data.main.pressure+" hPa");
+    $(".wind").text("Wind Speed: "+data.wind.speed+" m/h");
+  }, error: function(error){
+    $(".error-message").text("An error occured");
+  }})
 }
 
-// // JSONP Callback
-// function callback(data) {
-//   console.log(data);
-// }
-
-// Show Listings Of Pets
-function showAnimals(pets) {
-  const results = document.querySelector('#results')
-  // Clear First
-  results.innerHTML = ''
-  // Loop Through Pets
-  pets.forEach((pet) => {
-    // console.log(pet);
-    const div = document.createElement('div')
-    div.classList.add('card', 'card-body', 'mb-3')
-    div.innerHTML = `
-      <div class="row">
-        <div class="col-sm-6">
-          <h4>${pet.name.$t} (${pet.age.$t})</h4>
-          <p class="text-secondary">${pet.breeds.breed.$t}</p>
-          <p>${pet.contact.address1.$t} ${pet.contact.city.$t}
-					${pet.contact.state.$t} ${pet.contact.zip.$t}</p>
-          <ul class="list-group">
-            <li class="list-group-item">Phone: ${pet.contact.phone.$t}</li>
-            ${
-              pet.contact.email.$t
-                ? `<li class="list-group-item">Email:
-								${pet.contact.email.$t}</li>`
-                : ``
-            }
-            <li class="list-group-item">Shelter ID: ${pet.shelterId.$t}</li>
-          </ul>
-        </div>
-        <div class="col-sm-6 text-center">
-          <img class="img-fluid rounded-circle mt-2"
-					src="${pet.media.photos.photo[3].$t}">
-        </div>
-      </div>
-    `
-
-    results.appendChild(div)
-  })
+function searchWeather(){
+  var searchQuery = $(".search").val();
+  getWeather(searchQuery);
 }
+
