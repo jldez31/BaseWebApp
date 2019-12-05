3080167d972c9b15a583e30b7da96415e4886cec
Justine Dezember <justinedezember@Justines-Air.attlocal.net>
2019-12-04 19:04:28 -0500

    merge

--- project/js/main.js
+++ project/js/main.js
@@ -2,17 +2,22 @@ function getWeather(searchQuery) {
   var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=imperial&APPID="+apiKey;
 
   $(".city").text("");
+  $(".country").text("");
+  $(".conditions").text("");
   $(".temp").text("");
   $(".pressure").text("");
-    $(".wind").text("");
+  $(".wind").text("");
+  
 
   $.ajax(url,{success: function(data){
     $(".city").text("City: "+data.name);
+    $(".country").text("Country ID: "+data.sys.country);
+    $(".conditions").text("Conditions: "+data.weather.main);
     $(".temp").text("Temperature: "+data.main.temp+" *F");
     $(".pressure").text("Pressure: "+data.main.pressure+" hPa");
     $(".wind").text("Wind Speed: "+data.wind.speed+" m/h");
   }, error: function(error){
-    $(".error-message").text("An error occured");
+    $(".error-message").text("Please check for spelling errors and try again");
   }})
 }
 
